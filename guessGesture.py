affb400aff18ea4ed519f0653c7ee482001cbb03
Sowmya R <sowmyas@baasha.net>
2021-08-25 10:19:08 -0400

    Update guessGesture.py

--- guessGesture.py
+++ guessGesture.py
@@ -1,12 +1,19 @@
+import os
 import numpy as np
 import cv2 as cv
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter,ImageTk
 from keras.models import Sequential
-import tkinter as tk
+from tkinter import *
 import tensorflow as tf
 from tensorflow import keras
 
+guessLabel = None
+
 def guess():
+    if guessLabel is None:
+        guessLabel = Label(frame, text = "")
+    # guessLabel.pack()
+    
     vdo = cv.VideoCapture(0+cv.CAP_DSHOW)
     retval,userImg = vdo.read()
     vdo.release()
@@ -14,47 +21,63 @@ def guess():
     cv.destroyAllWindows()
     if retval != True:
         print("Can't read frame")
-        return
-    userImg = cv.cvtColor(userImg, cv.COLOR_BGR2GRAY)
-    userImg = cv.resize(userImg, (48, 36), interpolation = cv.INTER_CUBIC)
+        guessLabel = Label(frame, text = "cant read")
+        guessLabel.pack()
+        return 5
+    userImg = cv.cvtColor(userImg, cv.COLOR_BGR2GRAY)    
+    # blue,green,red = cv.split(userImg)
+    # img = cv.merge((red,green,blue))
+    # im = Image.fromarray(img)
+    # imgtk = ImageTk.PhotoImage(image=im)
+
     cv.imshow('frame',userImg)
+    userImg = cv.resize(userImg, (48, 36), interpolation = cv.INTER_CUBIC)
 
     np_img = np.asarray(userImg)
-    
-    # should you delete row / column? the 3rd argument in this delete function can only be 0=row, 1-=col
-    # you mentioned here 2 which is  out of bounds
-    np_img = np.delete(np_img, 2, 2)
-    np_img = np.delete(np_img, 1, 2)
-    np_img = np.reshape(np_img, (36, 48, 1))
+    # print(np_img)
+    # np_img = np.delete(np_img, 2, 2)
+    # np_img = np.delete(np_img, 1, 2)
+    np_img = np.reshape(np_img, (1, 36, 48, 1))
 
-    model = keras.models.load_model('D:\\Python\\hand maching\\test-model-epoch5.h5')
+    model = keras.models.load_model(os.getcwd() + '\\test-model-epoch5.h5')
     probability_model = Sequential([model, tf.keras.layers.Softmax()])
     predictions = probability_model.predict(np_img)
-    np.argmax(predictions[0])
-    if predictions[0]==0:
-        guess = tk.Label(frame, text = "Palm(Vertical)")
-    elif predictions[0]==1:
-        guess = tk.Label(frame, text = "Palm(Horizontal)")
-    elif predictions[0]==2:
-        guess = tk.Label(frame, text = "1 finger")
-    elif predictions[0]==3:
-        guess = tk.Label(frame, text = "Fist")
-    elif predictions[0]==4:
-        guess = tk.Label(frame, text = "Thumbs Up")
-    guess.pack()
+    print(predictions)
+    np.argmax(predictions)
+    print("argmax: ",np.argmax(predictions))
+    # guessLabel.destroy()
+    if np.argmax(predictions[0])==0:
+        # guessLabel = Label(frame, text = "Palm(Vertical)")
+        guessLabel.config(text="Palm(Vertical)")
+    elif np.argmax(predictions[0])==1:
+        # guessLabel = Label(frame, text = "Palm(Horizontal)")
+        guessLabel.config(text="Palm(Horizontal)")
+    elif np.argmax(predictions[0])==2:
+        # guessLabel = Label(frame, text = "1 finger")
+        guessLabel.config(text="1 finger")
+    elif np.argmax(predictions[0])==3:
+        # guessLabel = Label(frame, text = "Fist")
+        guessLabel.config(text="Fist")
+    elif np.argmax(predictions[0])==4:
+        # guessLabel = Label(frame, text = "Thumbs Up")
+        guessLabel.config(text="Thumbs Up")
+    # guessLabel.grid(column=1,row=1)
+    guessLabel.pack()
 
-if __name__=='__main__':
-    root = tk.Tk()
-    root.geometry("640x480")
-    frame = tk.Frame(root)
-    frame.pack()
 
-    label = tk.Label(frame, text = "Click to Guess your Hand Gesture!", command = guess())
-    label.pack()
+root = Tk()
+root.geometry("640x480")
 
-    testButton = tk.Button(frame, text = "Guess")
-    testButton.pack(padx = 3, pady = 3)
+# root.columnconfigure(0, minsize=250)
+# root.rowconfigure([0, 1], minsize=100)
+frame = Frame(root)
+frame.pack()
+label = Label(frame, text = "Click to Guess your Hand Gesture!")
+label.pack()
 
-    root.title("Test")
-    root.mainloop()
+testButton = Button(frame, text = "Guess", command = guess)
+# testButton.grid(row=0, column=1, sticky="w")
+testButton.pack(padx = 3, pady = 3)
 
+root.title("Guess Gesture")
+root.mainloop()
